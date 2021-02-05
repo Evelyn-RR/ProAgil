ee0f2226d37352a54c3a37bf29da4449a6fbf90c
Evelyn-RR <vilyn.rodrigues8901@gmail.com>
2021-02-05 14:59:50 -0300

    Alteracao front

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -2,6 +2,8 @@
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
 
@@ -9,7 +11,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { EventoService } from './_services/evento.service';
@@ -28,11 +30,14 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     EventoService,
--- ProAgil-App/src/app/evento/evento.component.ts
+++ ProAgil-App/src/app/evento/evento.component.ts
@@ -2,6 +2,13 @@ import { Evento } from './../_models/Evento';
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from '../_services/evento.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { ptBrLocale } from 'ngx-bootstrap/locale';
+import { templateJitUrl } from '@angular/compiler';
+defineLocale('pt-br', ptBrLocale);
 
 @Component({
   selector: 'app-evento',
@@ -13,18 +20,28 @@ export class EventoComponent implements OnInit {
 
   eventosFiltrados: Evento[];
   eventos: Evento[];
+
+  evento: Evento;
+  modoSalvar = 'post';
+
   imagemLargura = 80;
   imagemMargem = 2;
   mostrarImagem = false;
-  modalRef: BsModalRef;
+  registerForm: FormGroup;
+  bodyDeletarEvento = '';
+
+    // tslint:disable-next-line: variable-name
+  _filtroLista = '';
 
   constructor(
     private eventoService: EventoService,
-    private modalService: BsModalService
-    ) { }
+    private modalService: BsModalService,
+    private fb: FormBuilder,
+    private localeService: BsLocaleService
+    ) {
+      this.localeService.use('pt-br');
+    }
 
-  // tslint:disable-next-line: variable-name
-  _filtroLista = '';
   get filtroLista(): string {
     return this._filtroLista;
   }
@@ -33,11 +50,42 @@ export class EventoComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
+  editarEvento(evento: Evento, template: any){
+    this.modoSalvar = 'put';
+    this.openModal(template);
+    this.evento = evento;
+    this.registerForm.patchValue(evento);
+  }
+
+  novoEvento(template: any){
+    this.modoSalvar = 'post';
+    this.openModal(template);
+  }
+
+  excluirEvento(evento: Evento, template: any) {
+    this.openModal(template);
+    this.evento = evento;
+    this.bodyDeletarEvento = `Tem certeza que deseja excluir o Evento: ${evento.tema}, Código: ${evento.id}`;
+  }
+
+  confirmeDelete(template: any) {
+    this.eventoService.deleteEvento(this.evento.id).subscribe(
+      () => {
+          template.hide();
+          this.getEventos();
+        }, error => {
+          console.log(error);
+        }
+    );
+  }
+
+  openModal(template: any) {
+    this.registerForm.reset();
+    template.show();
   }
 
   ngOnInit() {
+    this.validation();
     this.getEventos();
   }
 
@@ -50,6 +98,44 @@ export class EventoComponent implements OnInit {
 
   alterarImagem() {
     this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  validation() {
+    this.registerForm = this.fb.group({
+      tema: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
+      imagemURL: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  salvarAlteracao(template: any){
+    if (this.registerForm.valid){
+      if (this.modoSalvar === 'post'){
+        this.evento = Object.assign({}, this.registerForm.value);
+        this.eventoService.postEvento(this.evento).subscribe(
+          () => {
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      } else {
+        this.evento = Object.assign({id: this.evento.id}, this.registerForm.value);
+        this.eventoService.putEvento(this.evento).subscribe(
+          () => {
+            template.hide();
+            this.getEventos();
+          }, error => {
+            console.log(error);
+          }
+        );
+      }
+    }
   }
 
   getEventos() {
